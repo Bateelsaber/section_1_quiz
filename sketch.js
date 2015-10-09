f9a8520d3a5952eaed6b007c1b6f35a362261a2b
Bateelsaber <sabeb234@newschool.edu>
2015-10-09 16:40:56 -0400

    setting up background

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
+	noStroke();
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 50);
+	// draw background
+	background(0, 0, 60);
 
-	// set the drawing style
-	fill(100, 100, 100);
-	stroke(255, 255, 255);
+	fill(0, 100, 50);
+	rect(0, 280, 640, 300)
+	for (var i = 0; i < 10; i++) {
+		fill(0, 60, 30);
+		triangle(0, 280, 30, 190, 60, 280);
+	}
+	fill(150, 150, 150);
+	ellipse(mouseX, mouseY, 70, 70);
+	ellipse(mouseX, mouseY + 30, 150, 50);
+	for (var i = 0; i < 5; i++) {
+		fill(100, 100, 100);
+		ellipse(mouseX - 72 + i * 35, mouseY + 30, 10, 10);
+	}
 
-	// draw an ellipse
-	ellipse(320, 180, 100, 100);
+
 }
